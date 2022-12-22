000137862cbc370806c928a4b2aeb716351176aa
kuze <370415788@qq.com>
2022-12-22 16:22:18 +0800

    no message

--- lab-go-kafka-client/main.go
+++ lab-go-kafka-client/main.go
@@ -1,9 +1,45 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
 	// testNormal()
 	// testTransaction()
 	// testIdempotence()
-	//testIdempotenceErr()
-	testTransactionRestart()
+	// testIdempotenceErr()
+	// testTransactionRestart()
+
+	stopCtx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	consumer := createTransactionConsumer("pass3")
+	defer consumer.Close()
+
+	count := 0
+
+	for {
+		select {
+		case sig := <-stopCtx.Done():
+			log.Printf("Consumer Caught signal %v: terminating\n", sig)
+			return
+		default:
+			msg, err := consumer.ReadMessage(-1)
+
+			if err != nil {
+				log.Printf("接收消息时发生错误 %v \n", err)
+				continue
+			}
+
+			if msg != nil {
+				count++
+				log.Printf("当前一共收到消息 %d 条", count)
+			}
+		}
+	}
+
 }
