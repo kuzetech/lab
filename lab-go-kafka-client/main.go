073891f5a0ee08c9c2ae558b9d354e05823f630d
黄少威 <huangshaowei@xmfunny.com>
2023-01-12 18:08:22 +0800

    no message

--- lab-go-kafka-client/main.go
+++ lab-go-kafka-client/main.go
@@ -1,32 +1,52 @@
 package main
 
 import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
+	"sync"
 	"time"
 )
 
 func main() {
 
+	var wg sync.WaitGroup
+	var termChan = make(chan struct{}, 0)
+
 	consumer := createTransactionConsumer("data")
 
+	wg.Add(1)
 	go func() {
+		log.Printf("开始接收消息 \n")
 		for {
-			log.Printf("开始接收消息 \n")
-			msg, err := consumer.ReadMessage(-1)
+			select {
+			case <-termChan:
+				log.Printf("接收消息线程收到停止信号 \n")
+				wg.Done()
+				return
+			default:
+				msg, err := consumer.ReadMessage(100)
 
-			if err != nil {
-				log.Printf("接收消息时发生错误 %v \n", err)
-				continue
-			}
+				if err != nil {
+					if err.(kafka.Error).Code() == kafka.ErrTimedOut {
+						continue
+					} else {
+						log.Printf("接收消息时发生错误 %v \n", err)
+						continue
+					}
+				}
 
-			if msg != nil {
-				log.Printf("收到消息")
+				if msg != nil {
+					log.Printf("收到消息")
+				}
 			}
 		}
 	}()
 
-	time.Sleep(time.Second * 5)
-	log.Printf("5秒睡眠结束，程序开始关闭")
+	time.Sleep(time.Second * 3)
+	log.Printf("睡眠结束，程序开始关闭")
+
+	close(termChan)
+	wg.Wait()
 
 	err := consumer.Close()
 	if err != nil {
