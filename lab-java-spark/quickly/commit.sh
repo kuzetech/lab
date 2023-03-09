eb5c4b3f03e6a94dd423d781d79adb332f4d3b55
黄少威 <huangshaowei@xmfunny.com>
2023-03-09 17:43:45 +0800

    no message

--- lab-java-spark/quickly/commit.sh
+++ lab-java-spark/quickly/commit.sh
@@ -5,4 +5,4 @@ spark-submit \
 --master spark://master:7077 \
 --executor-memory 1G \
 --total-executor-cores 2 \
-/spark_src/study-spark-1.0-SNAPSHOT.jar
+/spark_src/lab-java-spark-1.0-SNAPSHOT.jar
