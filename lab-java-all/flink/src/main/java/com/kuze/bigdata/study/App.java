6b617da6a1edbb2a1765b3ed0e7f383793f6ba44
黄少威 <huangshaowei@xmfunny.com>
2023-09-26 18:39:50 +0800

    no message

---
+++ lab-java-all/flink/src/main/java/com/kuze/bigdata/study/App.java
@@ -0,0 +1,59 @@
+package com.kuze.bigdata.study;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        log.info("启动应用");
+
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+        log.info("传入的参数是：" + parameterTool.getProperties().toString());
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(5000);
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.seconds(10)));
+        env.getConfig().setGlobalJobParameters(parameterTool);
+
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics("input-topic")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStreamSource<String> dataStreamSource =
+                env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "source");
+
+        KafkaSink<String> sink = KafkaSink.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic("output-topic")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .build();
+
+        dataStreamSource.sinkTo(sink);
+
+        env.execute("test");
+
+    }
+}
