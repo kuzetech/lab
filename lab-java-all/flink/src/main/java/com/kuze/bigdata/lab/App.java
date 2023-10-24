322e0cd2f25c7c9bafe1b6861a94e94d595639c9
黄少威 <huangshaowei@xmfunny.com>
2023-10-24 18:25:27 +0800

    no message

---
+++ lab-java-all/flink/src/main/java/com/kuze/bigdata/lab/App.java
@@ -0,0 +1,15 @@
+package com.kuze.bigdata.lab;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.fromElements(Tuple2.of(1L, 3L), Tuple2.of(2L, 5L), Tuple2.of(1L, 7L), Tuple2.of(2L, 4L), Tuple2.of(1L, 2L))
+                .keyBy(value -> value.f0)
+                .flatMap(new CountWindowAverage())
+                .print();
+        env.execute("test");
+    }
+}
