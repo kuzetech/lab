2941a5df604c2f4622d914b35e260d99a7bdd08c
黄少威 <huangshaowei@xmfunny.com>
2023-12-25 10:54:14 +0800

    no message

--- lab-java-all/vertx/src/main/java/org/example/App.java
+++ lab-java-all/vertx/src/main/java/org/example/App.java
@@ -1,34 +1,44 @@
 package org.example;
 
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
+import io.vertx.core.*;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
         VertxOptions options = new VertxOptions();
+        options.setEventLoopPoolSize(2);
         Vertx vertx = Vertx.vertx(options);
 
-        KafkaProducerVerticle kafkaProducerVerticle = new KafkaProducerVerticle();
-        Future<String> deployed = vertx.deployVerticle(kafkaProducerVerticle);
-        deployed.onComplete(r -> {
-            if (r.succeeded()) {
-                log.info("deployed kafka producer success");
-                Future<String> serverDeployed = vertx.deployVerticle(new ServerProducerVerticle(kafkaProducerVerticle.getProducer()));
-                serverDeployed.onComplete(sr -> {
-                    if (sr.succeeded()) {
-                        log.info("deployed server success");
-                    } else {
-                        log.error("deployed server error", sr.cause());
-                        vertx.close();
-                    }
-                });
-            } else {
-                log.error("deployed kafka producer error", r.cause());
-                vertx.close();
-            }
-        });
+        List<Verticle> verticleList = new ArrayList<>();
+
+
+        for (int i = 0; i < 5; i++) {
+            Verticle verticle = new MyVertical(i);
+            verticleList.add(verticle);
+        }
+
+        Future.all(verticleList.stream().map(vertx::deployVerticle).collect(Collectors.toList())).result();
+    }
+
+    private static class MyVertical extends AbstractVerticle implements Verticle {
+
+        private final int i;
+
+        public MyVertical(int i) {
+            this.i = i;
+        }
+
+
+        @Override
+        public void start() {
+            getVertx().setTimer(0, l -> {
+                System.out.println(this.i + " done at " + Thread.currentThread().getId() + " " + Thread.currentThread().getName());
+            });
+        }
     }
 }
---
+++ lab-java-all/vertx/src/main/java/org/example/MultiServerApp.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MultiServerApp {
+    public static void main(String[] args) {
+        VertxOptions options = new VertxOptions();
+        Vertx vertx = Vertx.vertx(options);
+
+        DeploymentOptions deploymentOptions = new DeploymentOptions();
+        deploymentOptions.setInstances(3);
+        Future<String> deployed = vertx.deployVerticle(ServerVerticle.class, deploymentOptions);
+        deployed.onComplete(r -> {
+            if (r.succeeded()) {
+                log.info("1 success");
+            } else {
+                log.error("1 fail, {}", r.cause().toString());
+            }
+        });
+
+
+    }
+}
