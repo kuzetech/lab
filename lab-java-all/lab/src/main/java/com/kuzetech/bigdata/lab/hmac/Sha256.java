9cf32a83d1043312f9c02ee11e681f5cded60600
黄少威 <huangshaowei@xmfunny.com>
2023-12-28 16:33:50 +0800

    no message

--- lab-java-all/lab/src/main/java/com/kuzetech/bigdata/lab/hmac/Sha256.java
+++ lab-java-all/lab/src/main/java/com/kuzetech/bigdata/lab/hmac/Sha256.java
@@ -14,18 +14,23 @@ public class Sha256 {
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
 
         String strContent = "{\"batchId\": \"123\",\"messages\":[{\"type\": \"UserMutation\",\"data\": {\"#event\": \"login\",\"#identify\": \"user-fake428566\",\"#log_id\": \"f6399633-ce17-494e-a943-cec1873d5bdd\",\"#operate\": \"set\",\"#time\": 1699609490679,\"properties\": {\"#device_id\": \"device-fake1548091\",\"level\": 4509781237501530015,\"sex\": \"男\",\"user_last_login_time\": 1699609490679}}}]}";
-        System.out.println(extracted(strContent.getBytes(StandardCharsets.UTF_8)));
+        System.out.println(extracted("demo", "123", "123", strContent.getBytes(StandardCharsets.UTF_8)));
     }
 
-    public static String extracted(byte[] content) throws NoSuchAlgorithmException, InvalidKeyException {
+    public static String extracted(
+            String accessId,
+            String nonce,
+            String timestamp,
+            byte[] content
+    ) throws NoSuchAlgorithmException, InvalidKeyException {
         Mac hmacSha256 = Mac.getInstance("HmacSHA256");
         hmacSha256.init(new SecretKeySpec("secret".getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
 
         byte[] methodBytes = "POST".getBytes(StandardCharsets.UTF_8);
         byte[] all = ArrayUtils.addAll(methodBytes, "/v1/collect".getBytes(StandardCharsets.UTF_8));
-        all = ArrayUtils.addAll(all, "demo".getBytes(StandardCharsets.UTF_8));
-        all = ArrayUtils.addAll(all, "123".getBytes(StandardCharsets.UTF_8));
-        all = ArrayUtils.addAll(all, "123".getBytes(StandardCharsets.UTF_8));
+        all = ArrayUtils.addAll(all, accessId.getBytes(StandardCharsets.UTF_8));
+        all = ArrayUtils.addAll(all, nonce.getBytes(StandardCharsets.UTF_8));
+        all = ArrayUtils.addAll(all, timestamp.getBytes(StandardCharsets.UTF_8));
         all = ArrayUtils.addAll(all, content);
 
         byte[] serverSign = hmacSha256.doFinal(all);
--- lab-java-all/lab/src/main/java/com/kuzetech/bigdata/lab/http/App.java
+++ lab-java-all/lab/src/main/java/com/kuzetech/bigdata/lab/http/App.java
@@ -1,38 +1,47 @@
-package com.kuzetech.bigdata.lab.okhttp;
+package com.kuzetech.bigdata.lab.http;
 
 import com.kuzetech.bigdata.lab.gzip.GzipApp;
 import com.kuzetech.bigdata.lab.hmac.Sha256;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+@Slf4j
 public class App {
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        //String content = "{\"batchId\":\"123\",\"messages\":[{\"type\":\"event\",\"data\":{\"a\":3,\"b\":\"3\"}}]}";
-        String content = "{\"batchId\":\"123\",\"messages\":[{\"a\":3,\"b\":\"3\"}]}";
+        String content = "{\"batchId\":\"123\",\"messages\":[{\"type\":\"event\",\"data\":{\"a\":3,\"b\":\"3\"}}]}";
         byte[] gzipContent = GzipApp.compress(content);
         RequestBody body = RequestBody.create(gzipContent, JSON);
 
         OkHttpClient client = new OkHttpClient.Builder().build();
+        String accessId = "demo";
+        String nonce = "123";
+        String timestamp = "123";
+
         Request request = new Request.Builder()
                 .post(body)
-                .addHeader("X-Signature", Sha256.extracted(gzipContent))
-                .addHeader("X-Timestamp", "123")
-                .addHeader("X-AccessKeyId", "demo")
-                .addHeader("X-Nonce", "123")
+                .addHeader("X-Signature", Sha256.extracted(accessId, nonce, timestamp, gzipContent))
+                .addHeader("X-Timestamp", timestamp)
+                .addHeader("X-AccessKeyId", accessId)
+                .addHeader("X-Nonce", nonce)
                 .addHeader("Content-Encoding", "gzip")
                 .url("http://localhost:8080/v1/collect")
                 .build();
 
         Call call = client.newCall(request);
-        Response response = call.execute();
-        assert response.body() != null;
-        System.out.println(response.code());
-        System.out.println(response.body().string());
+        Response res = call.execute();
+        assert res.body() != null;
+        if (res.isSuccessful()) {
+            log.info("success, message is {}", res.body().string());
+        } else {
+            log.info("fail，code is {}, message is {}", res.code(), res.body().string());
+        }
+
     }
 }
