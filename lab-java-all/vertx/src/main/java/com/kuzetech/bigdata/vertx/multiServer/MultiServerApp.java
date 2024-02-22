79a52af4472d9119c0debd0783dd3bf97f28f024
黄少威 <huangshaowei@xmfunny.com>
2024-02-22 18:33:18 +0800

    no message

--- lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/multiServer/MultiServerApp.java
+++ lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/multiServer/MultiServerApp.java
@@ -4,22 +4,50 @@ import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MultiServerApp {
     public static void main(String[] args) {
-        VertxOptions options = new VertxOptions();
-        Vertx vertx = Vertx.vertx(options);
+        VertxOptions vertxOptions = new VertxOptions();
+        VertxPrometheusOptions vertxPrometheusOptions = new VertxPrometheusOptions();
+        vertxPrometheusOptions.setPublishQuantiles(true);
+        vertxPrometheusOptions.setEnabled(true);
+
+        MicrometerMetricsOptions micrometerMetricsOptions = new MicrometerMetricsOptions();
+        micrometerMetricsOptions.setPrometheusOptions(vertxPrometheusOptions);
+        micrometerMetricsOptions.setEnabled(true);
+
+        vertxOptions.setMetricsOptions(micrometerMetricsOptions);
+        Vertx vertx = Vertx.vertx(vertxOptions);
 
         DeploymentOptions deploymentOptions = new DeploymentOptions();
         deploymentOptions.setInstances(3);
-        Future<String> deployed = vertx.deployVerticle(ServerVerticle.class, deploymentOptions);
-        deployed.onComplete(r -> {
+
+        JsonObject config = new JsonObject();
+        Metadata metadata = new Metadata();
+        metadata.setName("aaa");
+        config.put("metadata", metadata);
+        deploymentOptions.setConfig(config);
+
+        Future<String> serverDeployed = vertx.deployVerticle(ServerVerticle.class, deploymentOptions);
+        serverDeployed.onComplete(r -> {
             if (r.succeeded()) {
-                log.info("deploy success");
+                log.info("deploy server success");
             } else {
-                log.error("deploy fail, {}", r.cause().toString());
+                log.error("deploy server fail, {}", r.cause().toString());
+            }
+        });
+
+        Future<String> metadataDeployed = vertx.deployVerticle(MetadataVerticle.class, new DeploymentOptions().setInstances(1));
+        metadataDeployed.onComplete(r -> {
+            if (r.succeeded()) {
+                log.info("deploy metadata success");
+            } else {
+                log.error("deploy metadata fail, {}", r.cause().toString());
             }
         });
 
--- lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/multiServer/ServerVerticle.java
+++ lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/multiServer/ServerVerticle.java
@@ -1,25 +1,83 @@
 package com.kuzetech.bigdata.vertx.multiServer;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.kafka.KafkaClientMetrics;
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.Status;
+import io.vertx.ext.web.AllowForwardHeaders;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.kafka.client.producer.KafkaProducer;
+import io.vertx.kafka.client.producer.KafkaProducerRecord;
+import io.vertx.kafka.client.producer.RecordMetadata;
+import io.vertx.micrometer.PrometheusScrapingHandler;
+import io.vertx.micrometer.backends.BackendRegistries;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class ServerVerticle extends AbstractVerticle {
 
     private HttpServer server;
+    private Metadata metadata;
 
     @Override
     public void start(Promise<Void> startPromise) {
+        metadata = (Metadata) config().getValue("metadata");
+        log.info("初始化 server，metadata 为 " + metadata.getName());
+
+        Map<String, String> producerConfig = new HashMap<>();
+        producerConfig.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerConfig.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerConfig.put("compression.type", "lz4");
+        producerConfig.put("bootstrap.servers", "localhost:9092");
+        KafkaProducer<String, String> producer = KafkaProducer.createShared(vertx, "producer", producerConfig);
+
+        vertx.eventBus().consumer("news.uk.sport", message -> {
+            Metadata m = (Metadata) message.body();
+            log.info("I have received a message: " + m.getName());
+            metadata = m;
+        });
+
+        MeterRegistry registry = BackendRegistries.getDefaultNow();
+        registry.config().meterFilter(MeterFilter.accept());
+        new KafkaClientMetrics(producer.unwrap()).bindTo(registry);
+
         server = vertx.createHttpServer();
-        Router router = Router.router(vertx);
+        Router router = Router.router(vertx)
+                // 允许代理访问记录源IP
+                .allowForward(AllowForwardHeaders.X_FORWARD);
+
+        router.route().handler(BodyHandler.create().setBodyLimit(10000));
+
+        HealthCheckHandler healthCheckHandler = HealthCheckHandler.create(vertx);
+        healthCheckHandler.register("health", promise -> promise.complete(Status.OK()));
+
+        router.get("/_/health").handler(healthCheckHandler);
+        router.get("/metrics").handler(PrometheusScrapingHandler.create());
 
         router.get("/test")
                 .handler(ctx -> {
-                    log.info("test");
-                    ctx.json("success");
+                    String message = " done at " + Thread.currentThread().getId() + " " + Thread.currentThread().getName();
+                    KafkaProducerRecord<String, String> record = KafkaProducerRecord.create("topic", message);
+                    final Future<RecordMetadata> fut = producer.send(record);
+                    fut.onSuccess(event -> {
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        ctx.end("success");
+                    }).onFailure(event -> {
+                        ctx.end("fail");
+                    });
                 });
 
         // Now bind the server:
