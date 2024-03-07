6f5ba6c67171b70dc0719bb56e05cc92a002841e
黄少威 <huangshaowei@xmfunny.com>
2024-03-07 15:39:41 +0800

    no message

---
+++ lab-java-all/kafka/src/main/java/com/kuzetech/bigdata/kafka/commitAuto/SimpleConsumer.java
@@ -0,0 +1,42 @@
+package com.kuzetech.bigdata.kafka.commitAuto;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SimpleConsumer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "2000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+
+        while (true) {
+            try {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            } finally {
+                try {
+                    consumer.commitSync();
+                } finally {
+                    consumer.close();
+                }
+
+            }
+
+        }
+    }
+
+}
--- lab-java-all/kafka/src/main/java/com/kuzetech/bigdata/kafka/multiWorker/App.java
+++ lab-java-all/kafka/src/main/java/com/kuzetech/bigdata/kafka/multiWorker/App.java
@@ -1,15 +1,15 @@
 package com.kuzetech.bigdata.kafka.multiWorker;
 
-public class Main {
+public class App {
 
     /*
-    * 这种方式有丢失数据的风险
-    * 假设
-    * T1线程在t0时间消费分区0的位移=100的消息M1
-    * T2线程在t1时间消费分区0的位移=101的消息M2
-    * T2线程在t3时优先完成处理，于是上报位移101给Handler
-    * 但此时T1线程尚未处理完成。t4时handler提交位移101，之后T1线程发生错误，抛出异常导致位移100的消息消费失败，但由于位移已经提交到101，故消息丢失~。
-    * */
+     * 这种方式有丢失数据的风险
+     * 假设
+     * T1线程在t0时间消费分区0的位移=100的消息M1
+     * T2线程在t1时间消费分区0的位移=101的消息M2
+     * T2线程在t3时优先完成处理，于是上报位移101给Handler
+     * 但此时T1线程尚未处理完成。t4时handler提交位移101，之后T1线程发生错误，抛出异常导致位移100的消息消费失败，但由于位移已经提交到101，故消息丢失~。
+     * */
     public static void main(String[] args) {
         String brokerList = "localhost:9092";
         String topic = "test-topic";
