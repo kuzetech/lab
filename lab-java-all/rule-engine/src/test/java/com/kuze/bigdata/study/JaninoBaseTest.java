944df7159250578a036e3bd32476fde24067e1ba
huangshaowei <huangshaowei@xmfunny.com>
2024-04-17 22:59:47 +0800

    no message

--- lab-java-all/rule-engine/src/test/java/com/kuze/bigdata/study/JaninoBaseTest.java
+++ lab-java-all/rule-engine/src/test/java/com/kuze/bigdata/study/JaninoBaseTest.java
@@ -1,27 +1,65 @@
 package com.kuze.bigdata.study;
 
 import org.codehaus.commons.compiler.CompileException;
+import org.codehaus.janino.ExpressionEvaluator;
 import org.codehaus.janino.ScriptEvaluator;
+import org.codehaus.janino.SimpleCompiler;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class JaninoBaseTest {
 
     @Test
-    public void base() throws CompileException, InvocationTargetException {
+    public void expression() throws CompileException, InvocationTargetException {
+        ExpressionEvaluator ee = new ExpressionEvaluator();
+        ee.setParameters(new String[] { "a", "b" }, new Class[] { int.class, int.class });
+        ee.setExpressionType(int.class);
+        ee.cook("a + b");
+
+        Object result = ee.evaluate(new Object[] { 1, 2 });
+        System.out.println("Janino result=" + result);
+    }
+
+    @Test
+    public void script() throws CompileException, InvocationTargetException {
         ScriptEvaluator se = new ScriptEvaluator();
 
         se.cook(
                 ""
-                        + "static int method1(int a, int b) {\n"
-                        + "    return a+b;\n"
+                        + "static void method1() {\n"
+                        + "    System.out.println(1);\n"
+                        + "}\n"
+                        + "\n"
+                        + "method1();\n"
+                        + "method2();\n"
+                        + "\n"
+                        + "static void method2() {\n"
+                        + "    System.out.println(2);\n"
                         + "}\n"
         );
 
-        Object[] args = {1, 2};
-        Object result = se.evaluate(args);
-        System.out.println("Result: " + result);
+        se.evaluate();
+    }
+
+    @Test
+    public void simple() throws CompileException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        SimpleCompiler compiler = new SimpleCompiler();
+        compiler.setParentClassLoader(JaninoBaseTest.class.getClassLoader());
+
+        String content = "public class Foo {\n" +
+                "    public void say() {\n" +
+                "        System.out.println(100);\n" +
+                "    }\n" +
+                "}";
+        compiler.cook(content);
+
+        Class<?> aClass = compiler.getClassLoader().loadClass("Foo");
+
+        Method sayMethod = aClass.getDeclaredMethod("say");
+        sayMethod.invoke(aClass.newInstance());
+
     }
 
 }
