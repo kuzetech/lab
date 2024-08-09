79117486520263372626b251907bcf8a8bd0b53b
黄少威 <huangshaowei@xmfunny.com>
2024-08-09 16:07:23 +0800

    no message

--- lab-java-all/lab-work/src/main/java/com/kuzetech/bigdata/flink/PullMetadataApp.java
+++ lab-java-all/lab-work/src/main/java/com/kuzetech/bigdata/flink/PullMetadataApp.java
@@ -1,8 +1,6 @@
 package com.kuzetech.bigdata.flink;
 
 
-import com.xmfunny.turbine.sdk.extension.config.ConfigService;
-import com.xmfunny.turbine.sdk.extension.config.DefaultConverter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -10,7 +8,7 @@ public class PullMetadataApp {
     private static final Log LOG = LogFactory.getLog(PullMetadataApp.class);
 
     public static void main(String[] args) throws Exception {
-        ConfigService configService = new ConfigService("http://localhost:8090",
+        /*ConfigService configService = new ConfigService("http://localhost:8090",
                 "turbine",
                 "turbine123456",
                 5);
@@ -18,6 +16,6 @@ public class PullMetadataApp {
         String s = configService.loadConfig("funnydb", "metadata-funnydb", new DefaultConverter());
         LOG.info(s);
 
-        configService.close();
+        configService.close();*/
     }
 }
