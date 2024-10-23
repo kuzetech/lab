5563c20d7203fb347f801520b30fad9e4fe0ee97
黄少威 <huangshaowei@xmfunny.com>
2024-10-23 16:40:58 +0800

    no message

---
+++ lab-java-all/flink20-sql/src/main/java/com/kuzetech/bigdata/flink20/DataGenToKafka.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kuzetech.bigdata.flink20;
+
+import org.apache.flink.connector.datagen.table.DataGenConnectorOptions;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class DataGenToKafka {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment execEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        execEnvironment.setParallelism(1);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(execEnvironment);
+
+        tableEnv.createTemporaryTable("GenTable", TableDescriptor.forConnector("datagen")
+                .schema(Schema.newBuilder()
+                        .column("f0", DataTypes.INT())
+                        .column("f1", DataTypes.STRING())
+                        .build())
+                .option(DataGenConnectorOptions.ROWS_PER_SECOND, 1L)
+                .build());
+
+        tableEnv.executeSql(
+                "CREATE TEMPORARY TABLE NormalKafkaTable (\n" +
+                        "  `f0` INT,\n" +
+                        "  `f1` STRING\n" +
+                        ") WITH (\n" +
+                        "  'connector' = 'kafka',\n" +
+                        "  'topic' = 'normal',\n" +
+                        "  'properties.bootstrap.servers' = 'localhost:9092',\n" +
+                        "  'properties.allow.auto.create.topics' = 'true',\n" +
+                        "  'properties.group.id' = 'testGroup',\n" +
+                        "  'scan.startup.mode' = 'earliest-offset',\n" +
+                        "  'format' = 'json'\n" +
+                        ")"
+        );
+
+        tableEnv.executeSql(
+                "CREATE TEMPORARY TABLE UpsertKafkaTable (\n" +
+                        "  f0 INT,\n" +
+                        "  f1 STRING,\n" +
+                        "  PRIMARY KEY (f0) NOT ENFORCED\n" +
+                        ") WITH (\n" +
+                        "  'connector' = 'upsert-kafka',\n" +
+                        "  'topic' = 'upsert',\n" +
+                        "  'properties.bootstrap.servers' = 'localhost:9092',\n" +
+                        "  'properties.allow.auto.create.topics' = 'true',\n" +
+                        "  'key.format' = 'json',\n" +
+                        "  'value.format' = 'json'\n" +
+                        ");"
+        );
+
+        Table genTable = tableEnv.from("GenTable");
+
+        genTable.insertInto("NormalKafkaTable").execute();
+        genTable.insertInto("UpsertKafkaTable").execute();
+
+    }
+}
---
+++ lab-java-all/flink20-sql/src/main/java/com/kuzetech/bigdata/flink20/DataGenToPrint.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kuzetech.bigdata.flink20;
+
+import org.apache.flink.connector.datagen.table.DataGenConnectorOptions;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.*;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class DataGenToPrint {
+
+    public static void main(String[] args) throws Exception {
+
+        /*EnvironmentSettings envSettings = EnvironmentSettings
+                .newInstance()
+                .inStreamingMode()
+                //.inBatchMode()
+                .build();
+        TableEnvironment tableEnv = TableEnvironment.create(envSettings);
+        // 默认的命名空间
+        tableEnv.useCatalog("default_catalog");
+        tableEnv.useDatabase("default_database");*/
+
+
+        StreamExecutionEnvironment execEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        execEnvironment.setParallelism(1);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(execEnvironment);
+
+        tableEnv.createTemporaryTable("GenTable", TableDescriptor.forConnector("datagen")
+                .schema(Schema.newBuilder()
+                        .column("f0", DataTypes.INT())
+                        .column("f1", DataTypes.STRING())
+                        .build())
+                .option(DataGenConnectorOptions.ROWS_PER_SECOND, 1L)
+                .build());
+
+        tableEnv.executeSql(
+                "CREATE TEMPORARY TABLE PrintTable (\n" +
+                        " f0 INT,\n" +
+                        " f1 STRING\n" +
+                        ") WITH (\n" +
+                        " 'connector' = 'print'\n" +
+                        ")"
+        );
+
+        Table genTable = tableEnv.from("GenTable");
+
+        TablePipeline pipeline = genTable.insertInto("PrintTable");
+
+        TableResult result = pipeline.execute();
+    }
+}
