9cc6ecf33db64e6830d8ac293f53e4a82a083a41
黄少威 <huangshaowei@xmfunny.com>
2024-11-28 18:46:53 +0800

    no message

---
+++ lab-java-all/lab-json/src/main/java/com/kuzetech/bigdata/json/VertxValidatorApp.java
@@ -0,0 +1,41 @@
+package com.kuzetech.bigdata.json;
+
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
+import io.vertx.json.schema.*;
+import io.vertx.json.schema.common.dsl.ObjectSchemaBuilder;
+import io.vertx.json.schema.common.dsl.Schemas;
+
+import java.io.InputStream;
+
+public class VertxValidatorApp {
+    public static void main(String[] args) {
+
+        ObjectSchemaBuilder objectSchemaBuilder =
+                Schemas.objectSchema()
+                        .requiredProperty("id", Schemas.intSchema())
+                        .requiredProperty("name", Schemas.stringSchema())
+                        .requiredProperty("money", Schemas.numberSchema())
+                        .allowAdditionalProperties(false);
+
+        JsonSchema schema = JsonSchema.of(objectSchemaBuilder.toJson());
+
+        JsonSchemaOptions jsonSchemaOptions = new JsonSchemaOptions()
+                .setDraft(Draft.DRAFT4)
+                .setBaseUri("https://vertx.io");
+
+        Validator validator = Validator.create(schema, jsonSchemaOptions);
+
+        Object dataNode;
+        try (InputStream inputStream = VertxValidatorApp.class.getClassLoader().getResourceAsStream("data-student.json")) {
+            assert inputStream != null;
+            dataNode = Json.decodeValue(Buffer.buffer(inputStream.readAllBytes()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        OutputUnit result = validator.validate(dataNode);
+
+        System.out.println(result.getValid());
+    }
+}
---
+++ lab-java-all/lab-json/src/main/java/com/kuzetech/bigdata/json/domain/Student.java
@@ -0,0 +1,16 @@
+package com.kuzetech.bigdata.json.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private double money;
+}
