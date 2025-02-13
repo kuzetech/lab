02c35d3e32c317c4f9f05d42aa7949e30422a9a5
黄少威 <huangshaowei@xmfunny.com>
2025-02-13 18:56:46 +0800

    no message

--- lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/HandlerChainTest.java
+++ lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/HandlerChainTest.java
@@ -8,35 +8,30 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Random;
 
 @Slf4j
-public class NormalDeploy {
+public class HandlerChainTest {
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
 
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
 
-        router.get("/test")
+        router.get("/normal")
                 .handler(c -> {
                     log.info("1");
                     c.next();
                     log.info("3");
-
                 }).handler(c -> {
                     log.info("2");
                     c.end();
-                })
-        ;
+                });
 
-        router.get("/ffff")
+        router.get("/err1")
                 .handler(c -> {
                     log.info("1");
-                    if (new Random().nextBoolean()) {
-                        c.fail(400, new Exception("test"));
-                        return;
-                    }
+                    c.fail(400, new Exception("test"));
+                    log.info("没有 return 的话，失败后的业务代码还会执行");
                     c.next();
-                    log.info("不触发");
-
+                    log.info("5");
                 }).handler(c -> {
                     log.info("不触发");
                     c.end();
@@ -47,8 +42,29 @@ public class NormalDeploy {
                 }).failureHandler(c -> {
                     log.error("3");
                     c.end();
-                })
-        ;
+                });
+
+        router.get("/err2")
+                .handler(c -> {
+                    log.info("1");
+                    if (new Random().nextBoolean()) {
+                        c.fail(400, new Exception("test"));
+                        // 增加 return 后所有正常 handler 就不再执行了
+                        return;
+                    }
+                    c.next();
+                    log.info("5");
+                }).handler(c -> {
+                    log.info("不触发");
+                    c.end();
+                }).failureHandler(c -> {
+                    log.error("2");
+                    c.next();
+                    log.error("4");
+                }).failureHandler(c -> {
+                    log.error("3");
+                    c.end();
+                });
 
 
         server.requestHandler(router).listen(8080, "0.0.0.0").onComplete(res -> {
---
+++ lab-java-all/vertx/src/main/java/com/kuzetech/bigdata/vertx/QuicklyDeploy.java
@@ -0,0 +1,29 @@
+package com.kuzetech.bigdata.vertx;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class QuicklyDeploy {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+        HttpServer server = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+
+        router.get("/test").handler(c -> {
+            log.info(c.request().headers().toString());
+            c.end();
+        });
+
+        server.requestHandler(router).listen(8080, "0.0.0.0").onComplete(res -> {
+            if (res.succeeded()) {
+                log.info("Server is now listening!");
+            } else {
+                log.info("Failed to bind!");
+            }
+        });
+    }
+}
