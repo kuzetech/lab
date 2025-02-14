1990d03a333a04f9f5c2618c4690543df64f8f30
黄少威 <huangshaowei@xmfunny.com>
2025-02-14 11:34:07 +0800

    no message

---
+++ lab-java-all/flink18-stream-pulsar/src/main/java/com/kuzetech/bigdata/flink18/DataStreamJob.java
@@ -0,0 +1,12 @@
+package com.kuzetech.bigdata.flink18;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class DataStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.execute("Flink Java API Skeleton");
+    }
+}
