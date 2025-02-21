22a7355763e4517a8074ee05993a303f2c229f77
黄少威 <huangshaowei@xmfunny.com>
2025-02-21 13:39:29 +0800

    no message

--- lab-java-all/pulsar-client/src/main/java/com/kuzetech/bigdata/pulsar/BaseConsumer.java
+++ lab-java-all/pulsar-client/src/main/java/com/kuzetech/bigdata/pulsar/BaseConsumer.java
@@ -8,36 +8,27 @@ import org.apache.pulsar.client.api.PulsarClientException;
 public class BaseConsumer {
 
     public static void main(String[] args) throws PulsarClientException {
-        PulsarClient client = PulsarClient.builder()
-                .serviceUrl("pulsar://localhost:6650")
-                .build();
+        try (
+                PulsarClient client = PulsarClient.builder()
+                        .serviceUrl("pulsar://localhost:6650")
+                        .build();
 
-        Consumer consumer = client.newConsumer()
-                .topic("my-topic")
-                .subscriptionName("my-subscription")
-                .subscribe();
+                Consumer<byte[]> consumer = client.newConsumer()
+                        .topic("my-topic")
+                        .subscriptionName("my-subscription")
+                        .subscribe();
 
-
-        while (true) {
-            // Wait for a message
-            Message msg = consumer.receive();
-
-            try {
-                // Do something with the message
-                System.out.println("Message received: " + new String(msg.getData()));
-
-                // Acknowledge the message
-                consumer.acknowledge(msg);
-            } catch (Exception e) {
-                // Message failed to process, redeliver later
-                consumer.negativeAcknowledge(msg);
-            } finally {
-                break;
+        ) {
+            while (true) {
+                Message<byte[]> msg = consumer.receive();
+                try {
+                    System.out.println("Message received: " + new String(msg.getData()));
+                    consumer.acknowledge(msg);
+                } catch (Exception e) {
+                    consumer.negativeAcknowledge(msg);
+                    break;
+                }
             }
         }
-
-        consumer.close();
-        client.close();
-
     }
 }
--- lab-java-all/pulsar-client/src/main/java/com/kuzetech/bigdata/pulsar/BaseProducer.java
+++ lab-java-all/pulsar-client/src/main/java/com/kuzetech/bigdata/pulsar/BaseProducer.java
@@ -6,18 +6,16 @@ import org.apache.pulsar.client.api.PulsarClientException;
 
 public class BaseProducer {
     public static void main(String[] args) throws PulsarClientException {
-        PulsarClient client = PulsarClient.builder()
-                .serviceUrl("pulsar://localhost:6650")
-                .build();
+        try (
+                PulsarClient client = PulsarClient.builder()
+                        .serviceUrl("pulsar://localhost:6650")
+                        .build();
 
-        Producer<byte[]> producer = client.newProducer()
-                .topic("my-topic")
-                .create();
-
-        producer.send("My message".getBytes());
-
-
-        producer.close();
-        client.close();
+                Producer<byte[]> producer = client.newProducer()
+                        .topic("my-topic")
+                        .create();
+        ) {
+            producer.send("My message".getBytes());
+        }
     }
 }
