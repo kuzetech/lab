9c1da3a1e505e9de90d61ec32717bfbde60eea2d
黄少威 <huangshaowei@xmfunny.com>
2025-02-28 18:36:50 +0800

    no message

--- lab-java-all/pulsar-client/src/main/java/com/kuzetech/bigdata/pulsar/PulsarUtil.java
+++ lab-java-all/pulsar-client/src/main/java/com/kuzetech/bigdata/pulsar/PulsarUtil.java
@@ -1,15 +1,16 @@
 package com.kuzetech.bigdata.pulsar;
 
-import org.apache.pulsar.client.api.Consumer;
-import org.apache.pulsar.client.api.Producer;
-import org.apache.pulsar.client.api.PulsarClient;
-import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class PulsarUtil {
 
     public static PulsarClient getCommonPulsarClient() throws PulsarClientException {
         return PulsarClient.builder()
                 .serviceUrl("pulsar://localhost:6650")
+                .ioThreads(Runtime.getRuntime().availableProcessors())
+                .listenerThreads(Runtime.getRuntime().availableProcessors())
                 .listenerName("external")
                 .build();
     }
@@ -23,6 +24,15 @@ public class PulsarUtil {
 
     public static Producer<byte[]> getCommonProducer(PulsarClient client, String topic) throws PulsarClientException {
         return client.newProducer()
+                .producerName("lab-client")
+                .sendTimeout(30, TimeUnit.SECONDS)
+                .maxPendingMessages(0)
+                .blockIfQueueFull(false)
+                .enableBatching(true) // 仅对 sendAsync 有效
+                .batchingMaxPublishDelay(1, TimeUnit.MILLISECONDS)
+                .batchingMaxMessages(1000)
+                .batchingMaxBytes(128 * 1024) // 128KB
+                .compressionType(CompressionType.NONE)
                 .topic("public/default/" + topic)
                 .create();
     }
