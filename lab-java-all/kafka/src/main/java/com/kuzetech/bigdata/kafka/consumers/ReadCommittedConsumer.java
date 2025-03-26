98e308bf10a55b3d6c8533c52256197e0f8d36f6
黄少威 <huangshaowei@xmfunny.com>
2025-03-26 15:01:16 +0800

    no message

--- lab-java-all/kafka/src/main/java/com/kuzetech/bigdata/kafka/consumers/ReadCommittedConsumer.java
+++ lab-java-all/kafka/src/main/java/com/kuzetech/bigdata/kafka/consumers/ReadCommittedConsumer.java
@@ -1,6 +1,7 @@
 package com.kuzetech.bigdata.kafka.consumers;
 
 import com.kuzetech.bigdata.kafka.utils.ConsumerUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -8,15 +9,16 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import java.time.Duration;
 import java.util.Arrays;
 
+@Slf4j
 public class ReadCommittedConsumer {
 
     public static void main(String[] args) {
         try (KafkaConsumer<String, String> consumer = ConsumerUtil.CreateReadCommitted()) {
-            consumer.subscribe(Arrays.asList("test"));
+            consumer.subscribe(Arrays.asList("funnydb-ingest-receive"));
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
                 for (ConsumerRecord<String, String> record : records)
-                    System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                    log.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
             }
         }
     }
