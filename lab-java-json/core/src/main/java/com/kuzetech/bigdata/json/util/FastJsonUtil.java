c724d37edd0e9c0b37098775f26c2c1dcb17d192
黄少威 <huangshaowei@xmfunny.com>
2025-07-02 09:56:57 +0800

    no message

--- lab-java-json/core/src/main/java/com/kuzetech/bigdata/json/util/FastJsonUtil.java
+++ lab-java-json/core/src/main/java/com/kuzetech/bigdata/json/util/FastJsonUtil.java
@@ -8,8 +8,40 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+
 import static com.fasterxml.jackson.databind.DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS;
 
 public class FastJsonUtil {
-
+    //Map转成实体对象
+    public static Object mapToObject(Map<String, Object> map, Class<?> clazz) {
+        if (map == null) {
+            return null;
+        }
+        Object obj = null;
+        try {
+            obj = clazz.newInstance();
+            Field[] fields = obj.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                int mod = field.getModifiers();
+                if (Modifier.isStatic(mod) || Modifier.isFinal(mod)) {
+                    continue;
+                }
+                field.setAccessible(true);
+                String flag = (String) map.get(field.getName());
+                if(flag != null){
+                    if(flag.equals("false") || flag.equals("true")){
+                        field.set(obj, Boolean.parseBoolean(flag));
+                    }else{
+                        field.set(obj, map.get(field.getName()));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
 }
--- lab-java-json/fastjson-simple/src/test/java/com/kuzetech/bigdata/json/BaseTest.java
+++ lab-java-json/fastjson-simple/src/test/java/com/kuzetech/bigdata/json/BaseTest.java
@@ -6,6 +6,8 @@ import com.kuzetech.bigdata.json.domain.Account;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -28,5 +30,12 @@ class BaseTest {
         assertNull(noExistObject);
         String password = jsonObject.getString("password");
         System.out.println(password);
+    }
+
+    @Test
+    void StringToList() {
+        String jsonStr = "[{'password':'123123','username':'zhangsan'},{'password':'321321','username':'lisi'}]";
+        List<Account> accounts = JSON.parseArray(jsonStr, Account.class);
+        System.out.println("json字符串转List<Object>对象:"+accounts.toString());
     }
 }
