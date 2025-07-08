1007e5f7537487fe5be7948f16cfd246eb2f45d1
黄少威 <huangshaowei@xmfunny.com>
2025-07-08 16:52:53 +0800

    no message

---
+++ lab-flink-20/state-processor-track/src/main/java/com/kuzetech/bigdata/flink/HybridOperatorStateBuildSecondJob.java
@@ -0,0 +1,51 @@
+package com.kuzetech.bigdata.flink;
+
+import com.kuzetech.bigdata.flink.domain.DeviceOperatorKeyedState;
+import com.kuzetech.bigdata.flink.function.DeviceOperatorKeyedStateBootstrapper;
+import com.kuzetech.bigdata.flink.function.DeviceOperatorKeyedStateReaderFunction;
+import org.apache.commons.io.FileUtils;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
+import org.apache.flink.state.api.*;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.io.File;
+
+public class HybridOperatorStateBuildSecondJob {
+
+    public static final String OLD_SAVEPOINT_PATH = "file:///Users/huangsw/code/lab/lab-flink-20/state-processor-track/temp/savepoint/staging";
+    public static final String NEW_SAVEPOINT_PATH = "file:///Users/huangsw/code/lab/lab-flink-20/state-processor-track/temp/generate/HybridOperatorStateBuildSecondJob";
+    public static final String NEW_SAVEPOINT_DIRECTORY = "/Users/huangsw/code/lab/lab-flink-20/state-processor-track/temp/generate/HybridOperatorStateBuildSecondJob";
+
+    public static void main(String[] args) throws Exception {
+        FileUtils.deleteDirectory(new File(NEW_SAVEPOINT_DIRECTORY));
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(32);
+
+        SavepointReader savepoint = SavepointReader.read(
+                env,
+                OLD_SAVEPOINT_PATH,
+                new EmbeddedRocksDBStateBackend(true));
+
+        DataStream<DeviceOperatorKeyedState> deviceOperatorKeyedStateDataStream = savepoint.readKeyedState(
+                OperatorIdentifier.forUid("user-login-device-state"),
+                new DeviceOperatorKeyedStateReaderFunction(),
+                Types.STRING,
+                TypeInformation.of(DeviceOperatorKeyedState.class));
+
+        StateBootstrapTransformation<DeviceOperatorKeyedState> transformation = OperatorTransformation
+                .bootstrapWith(deviceOperatorKeyedStateDataStream)
+                .keyBy(o -> o.key)
+                .transform(new DeviceOperatorKeyedStateBootstrapper());
+
+        SavepointWriter
+                .fromExistingSavepoint(env, HybridOperatorStateBuildFirstJob.NEW_SAVEPOINT_PATH, new EmbeddedRocksDBStateBackend(true))
+                .withOperator(OperatorIdentifier.forUid("user-login-device-state"), transformation)
+                .write(NEW_SAVEPOINT_PATH);
+
+        env.execute("HybridOperatorStateBuildSecondJob");
+    }
+}
--- lab-flink-20/state-processor-track/src/main/java/com/kuzetech/bigdata/flink/HybridOperatorStateReadSecondJob.java
+++ lab-flink-20/state-processor-track/src/main/java/com/kuzetech/bigdata/flink/HybridOperatorStateReadSecondJob.java
@@ -1,7 +1,9 @@
 package com.kuzetech.bigdata.flink;
 
 import com.kuzetech.bigdata.flink.domain.DeviceOperatorKeyedState;
+import com.kuzetech.bigdata.flink.domain.DistinctOperatorKeyedState;
 import com.kuzetech.bigdata.flink.function.DeviceOperatorKeyedStateReaderFunction;
+import com.kuzetech.bigdata.flink.function.DistinctOperatorKeyedStateReaderFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
@@ -11,7 +13,7 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
 
-public class DeviceOperatorStateReadJob {
+public class HybridOperatorStateReadSecondJob {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -19,7 +21,7 @@ public class DeviceOperatorStateReadJob {
 
         SavepointReader savepoint = SavepointReader.read(
                 env,
-                DeviceOperatorStateBuildJob.NEW_SAVEPOINT_PATH,
+                HybridOperatorStateBuildSecondJob.NEW_SAVEPOINT_PATH,
                 new EmbeddedRocksDBStateBackend(true));
 
         DataStream<DeviceOperatorKeyedState> deviceOperatorKeyedStateDataStream = savepoint.readKeyedState(
@@ -28,15 +30,29 @@ public class DeviceOperatorStateReadJob {
                 Types.STRING,
                 TypeInformation.of(DeviceOperatorKeyedState.class));
 
-        DataStream<Long> totalCount = deviceOperatorKeyedStateDataStream
+        DataStream<Long> deviceCount = deviceOperatorKeyedStateDataStream
                 .map(x -> 1L)
                 .returns(Types.LONG)
                 .windowAll(GlobalWindows.createWithEndOfStreamTrigger())
                 .reduce(Long::sum);
 
-        totalCount.print(); // 3260604
+        deviceCount.print(); // 3260604
 
-        env.execute("DeviceOperatorStateReadJob");
+        DataStream<DistinctOperatorKeyedState> distinctOperatorKeyedStateDataStream = savepoint.readKeyedState(
+                OperatorIdentifier.forUid("filter-distinct"),
+                new DistinctOperatorKeyedStateReaderFunction(),
+                Types.STRING,
+                TypeInformation.of(DistinctOperatorKeyedState.class));
 
+        DataStream<Long> distinctCount = distinctOperatorKeyedStateDataStream
+                .map(x -> 1L)
+                .returns(Types.LONG)
+                .windowAll(GlobalWindows.createWithEndOfStreamTrigger())
+                .reduce(Long::sum);
+
+        distinctCount.print(); // 16662
+
+
+        env.execute("HybridOperatorStateReadSecondJob");
     }
 }
