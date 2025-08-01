e67cc1b37de389965e3b206ca94063c53f6f0d1f
黄少威 <huangshaowei@xmfunny.com>
2025-08-01 16:57:52 +0800

    no message

--- lab-flink-20/pulsar-transaction-validator/src/main/java/com/kuzetech/bigdata/flink/PulsarDuplicateKeyedProcessFunction.java
+++ lab-flink-20/pulsar-transaction-validator/src/main/java/com/kuzetech/bigdata/flink/PulsarDuplicateKeyedProcessFunction.java
@@ -17,40 +17,39 @@ public class PulsarDuplicateKeyedProcessFunction extends KeyedProcessFunction<St
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private static final ZoneId zoneId = ZoneId.of("Asia/Shanghai");
 
-    private ValueState<Integer> seenTimesState;
+    // 如果采用 Integer 类型，状态会大很多
+    private ValueState<Boolean> seenAlreadyState;
 
     @Override
     public void open(OpenContext openContext) throws Exception {
-        seenTimesState = getRuntimeContext().getState(
-                new ValueStateDescriptor<>("seenTimes", Integer.class, 0)
+        seenAlreadyState = getRuntimeContext().getState(
+                new ValueStateDescriptor<>("seenAlready", Boolean.class, false)
         );
     }
 
 
     @Override
     public void processElement(FunnyMessage msg, KeyedProcessFunction<String, FunnyMessage, String>.Context ctx, Collector<String> out) throws Exception {
-        Integer seenTimes = seenTimesState.value();
-        if (seenTimes == 0) {
+        Boolean seenAlready = seenAlreadyState.value();
+        if (seenAlready == null || Boolean.FALSE.equals(seenAlready)) {
+            seenAlreadyState.update(true);
             ctx.timerService().registerEventTimeTimer(msg.getIngestTime() + 300000);
-        }
-        seenTimesState.update(seenTimes + 1);
-    }
-
-    @Override
-    public void onTimer(long timestamp, KeyedProcessFunction<String, FunnyMessage, String>.OnTimerContext ctx, Collector<String> out) throws Exception {
-        Integer seenTimes = seenTimesState.value();
-        if (seenTimes > 1) {
+        } else {
+            seenAlreadyState.clear();
             LocalDateTime currentTime = Instant.ofEpochMilli(System.currentTimeMillis())
                     .atZone(zoneId)
                     .toLocalDateTime();
             String currentTimeStr = currentTime.format(formatter);
             out.collect(String.format(
-                    "Duplicate record, index=%s, count=%d, Current Time: %s",
+                    "Duplicate record, index=%s, Current Time: %s",
                     ctx.getCurrentKey(),
-                    seenTimes,
                     currentTimeStr
             ));
         }
-        seenTimesState.clear();
+    }
+
+    @Override
+    public void onTimer(long timestamp, KeyedProcessFunction<String, FunnyMessage, String>.OnTimerContext ctx, Collector<String> out) throws Exception {
+        seenAlreadyState.clear();
     }
 }
