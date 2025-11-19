55d2ff09992c03b2d5423651c3ccccee5b11b006
黄少威 <huangshaowei@xmfunny.com>
2025-11-19 17:24:20 +0800

    no message

---
+++ lab-flink-20/core/src/main/java/com/kuzetech/bigdata/flink/base/DataConstant.java
@@ -0,0 +1,14 @@
+package com.kuzetech.bigdata.flink.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataConstant {
+    public static final List<String> APP_NAME_LIST = new ArrayList<>();
+
+    static {
+        APP_NAME_LIST.add("app1");
+        APP_NAME_LIST.add("app2");
+        APP_NAME_LIST.add("app3");
+    }
+}
---
+++ lab-flink-20/pulsar/src/main/java/com/kuzetech/bigdata/flink/ProducerBaseJob.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kuzetech.bigdata.flink;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kuzetech.bigdata.flink.base.DataConstant;
+import com.kuzetech.bigdata.flink.base.FlinkUtil;
+import com.kuzetech.bigdata.flink.json.ObjectMapperInstance;
+import com.kuzetech.bigdata.flink.pulsar.PulsarConfig;
+import com.kuzetech.bigdata.flink.pulsar.PulsarSourceMessage;
+import com.kuzetech.bigdata.flink.pulsar.PulsarUtil;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.pulsar.sink.PulsarSinkBuilder;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.nio.charset.StandardCharsets;
+
+public class ProducerBaseJob {
+
+    private static final ObjectMapper OBJECT_MAPPER = ObjectMapperInstance.getInstance();
+
+    public static void main(String[] args) throws Exception {
+
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+        final StreamExecutionEnvironment env = FlinkUtil.initEnv(parameterTool);
+
+        final PulsarConfig pulsarConfig = PulsarConfig.generateFromParameterTool(parameterTool);
+
+        SingleOutputStreamOperator<String> sourceStream = env.fromData(DataConstant.APP_NAME_LIST)
+                .uid("source")
+                .name("source");
+
+        SingleOutputStreamOperator<PulsarSourceMessage> msgStream = sourceStream.map(u -> new PulsarSourceMessage(u.getBytes(StandardCharsets.UTF_8)));
+
+        PulsarSinkBuilder<PulsarSourceMessage> sinkBuilder = PulsarUtil.buildSinkBaseBuilder(pulsarConfig);
+
+        msgStream.sinkTo(sinkBuilder.build())
+                .uid("sink")
+                .name("sink");
+
+        env.execute(pulsarConfig.getJobName());
+    }
+}
