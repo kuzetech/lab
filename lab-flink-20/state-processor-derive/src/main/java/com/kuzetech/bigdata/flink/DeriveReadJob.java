f8db3623fe8237a8f66b5069eba525bbe95eb247
黄少威 <huangshaowei@xmfunny.com>
2025-12-08 09:49:03 +0800

    no message

--- lab-flink-20/state-processor-derive/src/main/java/com/kuzetech/bigdata/flink/DeriveReadJob.java
+++ lab-flink-20/state-processor-derive/src/main/java/com/kuzetech/bigdata/flink/DeriveReadJob.java
@@ -9,7 +9,6 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
 import org.apache.flink.state.api.OperatorIdentifier;
 import org.apache.flink.state.api.SavepointReader;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
 
@@ -17,56 +16,49 @@ public class DeriveReadJob {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(32);
 
         SavepointReader savepoint = SavepointReader.read(
                 env,
-                "file:///Users/huangsw/code/lab/lab-flink-17/state-processor-track/data/staging/derive",
+                "file:///Users/huangsw/code/lab/lab-flink-17/state-processor-derive/data/staging/derive",
                 new EmbeddedRocksDBStateBackend(true));
 
-        DataStream<IdentifyNewOperatorKeyedState> identifyDataStream = savepoint.readKeyedState(
-                OperatorIdentifier.forUid("derive-event-process"),
-                new IdentifyNewOperatorKeyedStateReaderFunction(),
-                Types.STRING,
-                TypeInformation.of(IdentifyNewOperatorKeyedState.class));
-        identifyDataStream
+        savepoint.readKeyedState(
+                        OperatorIdentifier.forUid("derive-event-process"),
+                        new IdentifyNewOperatorKeyedStateReaderFunction(),
+                        Types.STRING,
+                        TypeInformation.of(IdentifyNewOperatorKeyedState.class))
                 .map(x -> 1L)
                 .returns(Types.LONG)
                 .windowAll(GlobalWindows.createWithEndOfStreamTrigger())
                 .reduce(Long::sum)
                 .print("identifyDataStreamResult: "); //5809096
 
-        DataStream<AuOperatorKeyedState> dauDataStream = savepoint.readKeyedState(
-                OperatorIdentifier.forUid("derive-event-dau-process"),
-                new AuOperatorKeyedStateReaderFunction("dau-state"),
-                Types.STRING,
-                TypeInformation.of(AuOperatorKeyedState.class));
-        dauDataStream
-                .map(x -> 1L)
+        savepoint.readKeyedState(
+                        OperatorIdentifier.forUid("derive-event-dau-process"),
+                        new AuOperatorKeyedStateReaderFunction("dau-state"),
+                        Types.STRING,
+                        TypeInformation.of(AuOperatorKeyedState.class)).map(x -> 1L)
                 .returns(Types.LONG)
                 .windowAll(GlobalWindows.createWithEndOfStreamTrigger())
                 .reduce(Long::sum)
                 .print("dauDataStreamResult: "); //3907255
 
-        DataStream<AuOperatorKeyedState> wauDataStream = savepoint.readKeyedState(
-                OperatorIdentifier.forUid("derive-event-wau-process"),
-                new AuOperatorKeyedStateReaderFunction("wau-state"),
-                Types.STRING,
-                TypeInformation.of(AuOperatorKeyedState.class));
-        wauDataStream
+        savepoint.readKeyedState(
+                        OperatorIdentifier.forUid("derive-event-wau-process"),
+                        new AuOperatorKeyedStateReaderFunction("wau-state"),
+                        Types.STRING,
+                        TypeInformation.of(AuOperatorKeyedState.class))
                 .map(x -> 1L)
                 .returns(Types.LONG)
                 .windowAll(GlobalWindows.createWithEndOfStreamTrigger())
                 .reduce(Long::sum)
                 .print("wauDataStreamResult: "); //3907250
 
-        DataStream<AuOperatorKeyedState> mauDataStream = savepoint.readKeyedState(
-                OperatorIdentifier.forUid("derive-event-mau-process"),
-                new AuOperatorKeyedStateReaderFunction("mau-state"),
-                Types.STRING,
-                TypeInformation.of(AuOperatorKeyedState.class));
-        mauDataStream
-                .map(x -> 1L)
+        savepoint.readKeyedState(
+                        OperatorIdentifier.forUid("derive-event-mau-process"),
+                        new AuOperatorKeyedStateReaderFunction("mau-state"),
+                        Types.STRING,
+                        TypeInformation.of(AuOperatorKeyedState.class)).map(x -> 1L)
                 .returns(Types.LONG)
                 .windowAll(GlobalWindows.createWithEndOfStreamTrigger())
                 .reduce(Long::sum)
