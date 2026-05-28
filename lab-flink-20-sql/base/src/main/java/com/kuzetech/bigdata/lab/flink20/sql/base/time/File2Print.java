c000508e758683024b5abe87c022972c71c9858e
黄少威 <huangshaowei@xmfunny.com>
2026-05-28 11:43:56 +0800

    no message

--- lab-flink-20-sql/base/src/main/java/com/kuzetech/bigdata/lab/flink20/sql/base/time/File2Print.java
+++ lab-flink-20-sql/base/src/main/java/com/kuzetech/bigdata/lab/flink20/sql/base/time/File2Print.java
@@ -4,36 +4,38 @@ import com.kuzetech.bigdata.lab.flink20.sql.core.util.EnvironmentSettingsUtil;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableEnvironment;
 
-public class Gen2Print {
+import java.time.ZoneId;
+
+public class File2Print {
     public static void main(String[] args) {
         EnvironmentSettings settings = EnvironmentSettingsUtil.getEnvironmentSettings();
 
         TableEnvironment tableEnv = TableEnvironment.create(settings);
+        tableEnv.getConfig().setLocalTimeZone(ZoneId.of("Asia/Shanghai"));
 
         tableEnv.executeSql("""
                 CREATE TEMPORARY TABLE source (
-                    id INT,
-                    event_time TIMESTAMP,
+                    event STRING,
+                    event_time TIMESTAMP(3),
                     proc_time AS PROCTIME(),
                     WATERMARK FOR event_time AS event_time - INTERVAL '60' SECOND
                 ) WITH (
-                    'connector' = 'datagen',
-                    'scan.parallelism' = '1',
-                    'fields.id.kind' = 'random',
-                    'fields.id.min' = '1',
-                    'fields.id.max' = '10',
-                    'fields.ts.max-past' = '60000',
-                    'rows-per-second' = '1',
-                    'scan.watermark.emit.strategy'='on-event'
+                    'connector' = 'filesystem',
+                    'path' = './data/time/events.csv',
+                    'format' = 'csv',
+                    'csv.allow-comments' = 'true',
+                    'csv.field-delimiter' = ',',
+                    'csv.ignore-parse-errors' = 'false',
+                    'source.monitor-interval' = '10s'
                 )
                 """);
 
-
         tableEnv.executeSql("""
                 CREATE TEMPORARY TABLE sink (
-                    id  INT,
-                    event_time TIMESTAMP,
-                    proc_time TIMESTAMP
+                    window_start    TIMESTAMP(3),
+                    window_end  TIMESTAMP(3),
+                    event STRING,
+                    total   BIGINT
                 ) WITH (
                     'connector' = 'print'
                 )
@@ -42,8 +44,12 @@ public class Gen2Print {
         tableEnv.executeSql("""
                 INSERT INTO sink
                 SELECT
-                    *
-                FROM source
+                    window_start,
+                    window_end,
+                    event,
+                    count(1) AS total
+                FROM TABLE(TUMBLE(TABLE source, DESCRIPTOR(event_time), INTERVAL '1' MINUTES))
+                GROUP BY window_start, window_end, event;
                 """);
     }
 }
